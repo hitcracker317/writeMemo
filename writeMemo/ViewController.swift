d3dc9e592bfe178b6b2805e334984330e9346984
koseki <dainaso97@gmailcom>
2015-11-03 14:14:59 +0900

    first commit

---
+++ writeMemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  writeMemo
+//
+//  Created by 前田 晃良 on 2015/11/03.
+//  Copyright (c) 2015年 A.M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ writeMemoTests/writeMemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  writeMemoTests.swift
+//  writeMemoTests
+//
+//  Created by 前田 晃良 on 2015/11/03.
+//  Copyright (c) 2015年 A.M. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class writeMemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
