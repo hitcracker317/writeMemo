f1ebd65dfc14b6ba0c90f65ceae71c7399593a31
koseki <dainaso97@gmailcom>
2015-11-04 23:46:42 +0900

    メモのビューをタップしたらUITextViewを配置

--- writeMemo/viewController/MemoViewController.swift
+++ writeMemo/viewController/MemoViewController.swift
@@ -8,28 +8,59 @@
 
 import UIKit
 
-class MemoViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class MemoViewController: UIViewController,UITextViewDelegate {
+    
+    enum InputType: Int {
+        case InputTypeText = 0
+        case InputTypePaint
+        case InputTypeImage
+        case InputTypeMove
+        case InputTypeDelete
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    var inputType:InputType = InputType.InputTypeMove //初期inputTypeはmoveにしておく
+    
+    @IBOutlet weak var memoView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     }
-    */
-
+    
+    @IBAction func tapMemoView(sender: AnyObject) {
+        println(sender)
+        
+        var point = sender.locationOfTouch(0, inView: memoView)
+        println("タッチした座標:\(point)")
+        
+        
+        
+        
+        
+        
+        
+        
+        var textView:UITextView = UITextView(frame: CGRectMake(point.x - 12, point.y - 12, 24, 24))
+        textView.text = ""
+        textView.delegate = self
+        textView.backgroundColor = UIColor(red:0.95, green:0.95, blue:0.00, alpha:1.0)
+        memoView.addSubview(textView)
+        
+        textView.becomeFirstResponder()
+    }
+    
+    @IBAction func changeInputText(sender: AnyObject) {
+        //テキスト編集モードにチェンジ
+        inputType = InputType.InputTypeText
+    }
+    
+    // MARK: - TextViewDelegate
+    func textViewDidChange(textView: UITextView) {
+        //文字が入力された際、文字量に応じてTextViewのRect値を変更
+        var textRect:CGRect = textView.frame;
+        textRect.size.width = textView.contentSize.width
+        textRect.size.height = textView.contentSize.height
+        textView.frame = textRect;
+    }
+    
 }
