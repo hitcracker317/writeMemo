8bb5aaec682a4f1e81cbd889c5cd51bdf04340d1
koseki <dainaso97@gmailcom>
2015-11-05 16:50:26 +0900

    ペイントオプションビューのUIパーツ配置

---
+++ writeMemo/view/DrawOptionView.swift
@@ -0,0 +1,18 @@
+//
+//  DrawOptionView.swift
+//  writeMemo
+//
+//  Created by 前田 晃良 on 2015/11/05.
+//  Copyright (c) 2015年 A.M. All rights reserved.
+//
+
+import UIKit
+
+class DrawOptionView: UIView {
+
+    override func drawRect(rect: CGRect) {
+        
+        self.backgroundColor = UIColor(red:0.15, green:0.09, blue:0.36, alpha:1.0)
+    }
+
+}
