8e35f2c0a1a7f90a9239b8089fffb2e5b12a27d5
koseki <dainaso97@gmailcom>
2015-11-17 14:20:02 +0800

    キーボードにボタンを追加

--- writeMemo/view/InputTextView.swift
+++ writeMemo/view/InputTextView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class InputTextView: UIView {
+class InputTextView: UIView ,UITextViewDelegate{
 
     @IBOutlet weak var backInputTextView: UIView!
     @IBOutlet weak var scrollInputTextView: UIScrollView!
@@ -20,7 +20,59 @@ class InputTextView: UIView {
     
     func showInputTextView(){
         self.backInputTextView.backgroundColor = UIColor(red:0.20, green:0.20, blue:0.20, alpha:0.5)
-        
         inputTextView.becomeFirstResponder()
+        
+        self.prepareKeyBoard()
     }
+    
+    func prepareKeyBoard(){
+        //キーボードに色とフォントサイズを変更するボタンを設置
+        
+        //ボタンを配置するビューをキーボードに追加する
+        let keyBoardView:UIView = UIView(frame: CGRectMake(0,0,self.frame.width,50))
+        keyBoardView.backgroundColor = UIColor.lightGrayColor()
+        inputTextView.delegate = self
+        inputTextView.inputAccessoryView = keyBoardView
+        
+        //カラーを変更するボタンを設置
+        let changeColorButton:UIButton = UIButton()
+        changeColorButton.frame = CGRectMake(10, 3, 44, 44)
+        changeColorButton.backgroundColor = UIColor(red:0.98, green:0.87, blue:0.94, alpha:1)
+        changeColorButton.setTitle("カ", forState: .Normal)
+        changeColorButton.addTarget(self, action:"changeEditColor:", forControlEvents: .TouchUpInside)
+        keyBoardView.addSubview(changeColorButton)
+        //フォントサイズを変更するボタンを設置
+        let changeFontSizeButton:UIButton = UIButton()
+        changeFontSizeButton.frame = CGRectMake(20 + changeColorButton.frame.width, 3, 44, 44)
+        changeFontSizeButton.backgroundColor = UIColor(red:0.82, green:0.92, blue:0.97, alpha:1)
+        changeFontSizeButton.setTitle("フ", forState: .Normal)
+        changeFontSizeButton.addTarget(self, action:"changeEditFontSize:", forControlEvents: .TouchUpInside)
+        keyBoardView.addSubview(changeFontSizeButton)
+        
+        //キーボードを閉じてテキストの編集を終えるボタン
+        let finishButton:UIButton = UIButton()
+        finishButton.frame = CGRectMake(self.frame.width - 60, 0, 60, 50)
+        finishButton.backgroundColor = UIColor(red:0.92, green:0.38, blue:0.33, alpha:1)
+        finishButton.setTitle("完了", forState: .Normal)
+        finishButton.addTarget(self, action:"finishEditText:", forControlEvents: .TouchUpInside)
+        keyBoardView.addSubview(finishButton)
+        
+        //
+        
+    }
+    
+    func changeEditColor(sender:UIButton){
+        print("カラー")
+    }
+    func changeEditFontSize(sender:UIButton){
+        print("フォントサイズ")
+    }
+    
+    func finishEditText(sender:UIButton){
+        print("テキストの編集を終了")
+        //キーボードを閉じる
+        //このビューを閉じる
+        //memoViewControllerに入力したテキストの値を受け渡す
+    }
+    
 }
